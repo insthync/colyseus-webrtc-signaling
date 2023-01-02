9924ebfe519b77023442ef89847840b059e09314
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-01-02 23:19:33 +0700

    feat: improve codes

--- src/rooms/SignalingRoom.ts
+++ src/rooms/SignalingRoom.ts
@@ -8,29 +8,13 @@ export class SignalingRoom extends Room {
   }
 
   onCandidate(client: Client, message: any) {
-    const sessionId = message.sessionId;
-    const candidate = message.candidate;
-    const sdpMid = message.sdpMid;
-    const sdpMLineIndex = message.sdpMLineIndex;
-    this.broadcast("candidate", {
-      sessionId: sessionId,
-      candidate: candidate,
-      sdpMid: sdpMid,
-      sdpMLineIndex: sdpMLineIndex,
-    }, {
+    this.broadcast("candidate", message, {
       except: client
     });
   }
 
   onDesc(client: Client, message: any) {
-    const sessionId = message.sessionId;
-    const type = message.type;
-    const sdp = message.sdp;
-    this.broadcast("desc", {
-      sessionId: sessionId,
-      type: type,
-      sdp: sdp,
-    }, {
+    this.broadcast("desc", message, {
       except: client
     });
   }
